4390fa7ebb76bf6507e651f6c7c8d97484ef797e
Keith Damiani <keith@tighten.co>
2018-05-25 12:13:51 -0400

    Start ListPicker

--- src/components/category-index.vue
+++ src/components/category-index.vue
@@ -8,13 +8,47 @@
         </ActionBar>
 
         <StackLayout>
+            <ListPicker :items="category_array" v-model="selected" />
         </StackLayout>
     </Page>
 </template>
 
 <script>
-  export default {
-  };
+export default {
+    data() {
+        return {
+            categories: [
+                {
+                    id: 1,
+                    name: 'Category One',
+                },
+                {
+                    id: 2,
+                    name: 'Category Two',
+                },
+                {
+                    id: 3,
+                    name: 'Category Three',
+                },
+            ],
+            selected: null,
+        }
+    },
+
+    computed: {
+        category_array() {
+            return this.categories.map((category) => {
+                return category.name;
+            });
+        },
+    },
+
+    watch: {
+        selected(val) {
+            console.log('selected ' + val);
+        },
+    },
+};
 </script>
 
 <style scoped>
