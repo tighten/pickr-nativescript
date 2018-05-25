b9e63aed7b33d5e3ea269d6c573496444cef04ae
Keith Damiani <keith@tighten.co>
2018-05-25 13:49:57 -0400

    Picker working with advanced Pick Again™ functionality

--- src/components/category-pick.vue
+++ src/components/category-pick.vue
@@ -9,18 +9,21 @@
 
         <StackLayout>
             <Label class="h1 text-center p-y-10" :text="category.name"/>
+
+            <Label class="h1 text-center p-y-10" :text="random_item.name"/>
+            <Label class="h3 text-center p-y-10" :text="random_item.description"/>
+
+            <Button class="btn btn-primary" @tap="pickRandomItem">Pick Again!</Button>
         </StackLayout>
     </Page>
 </template>
 
 <script>
 export default {
-    props: {
-    },
-
     data() {
         return {
             category_id: this.$route.params.category_id,
+            random_item: {},
         }
     },
 
@@ -28,6 +31,19 @@ export default {
         category() {
             return this.$store.get('categories', this.category_id);
         },
+        items() {
+            return this.$store.get(['categories', this.category_id, 'items']);
+        },
+    },
+
+    methods: {
+        pickRandomItem() {
+            this.random_item = this.$_.sample(this.items) || {};
+        },
+    },
+
+    created() {
+        this.pickRandomItem();
     },
 };
 </script>
