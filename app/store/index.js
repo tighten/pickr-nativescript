76cd427423c0cdc70b455e94d8cb6ed6228c508e
Keith Damiani <keith@tighten.co>
2018-09-21 11:20:34 -0400

    Remove old store

---
+++ app/store/index.js
@@ -0,0 +1,111 @@
+import Vue from 'nativescript-vue';
+import Vuex from 'vuex';
+Vue.use(Vuex);
+
+import api from '../api';
+
+const state = {
+    errors: [],
+    categories: [],
+    selected_category_id: null,
+}
+
+const getters = {
+    selected_category(state) {
+        return state.categories.find(
+            category => category.id === state.selected_category_id
+        )
+    }
+}
+
+const mutations= {
+    setCategories(state, categories) {
+        state.categories = categories;
+    },
+    setCategory(state, payload) {
+        let category = state.categories.find(
+            category => category.id === payload.id
+        );
+
+        if (category) {
+            Object.assign(category, payload);
+        }
+    },
+    deleteCategory(state, id) {
+        let index = state.categories.findIndex(category => category.id === id);
+
+        if (index > -1) {
+            state.categories.splice(index, 1);
+            state.selected_category_id = null;
+        }
+    },
+    addCategory(state, payload) {
+        state.categories.push(Object.assign(payload, { items: [] }));
+        state.selected_category_id = payload.id;
+    },
+    addItem(state, payload) {
+        let category = state.categories.find(
+            category => category.id === state.selected_category_id
+        );
+
+        if (category) {
+            category.items.push(payload)
+        }
+    },
+    setSelectedCategory(state, id) {
+        state.selected_category_id = id;
+    },
+    clearErrors(state) {
+        state.errors = {};
+    },
+    setErrors(state, errors) {
+        state.errors = errors;
+    },
+}
+
+const actions = {
+    fetchCategories({commit}) {
+        api.get('api/categories')
+            .then(response => {
+                commit('setCategories', response.data);
+            }).catch(error => {
+                console.log('Category fetch ERROR', error);
+            });
+    },
+    createCategory({state, commit}, payload) {
+        api.post('api/categories', payload)
+            .then(response => {
+                commit('addCategory', response.data);
+            }).catch(error => {
+                console.log('Category create ERROR', error);
+            });
+    },
+    updateSelectedCategory({state, commit}, payload) {
+        api.put('api/categories/' + state.selected_category_id, { name: payload.name })
+            .then(response => {
+                commit('setCategory', response.data);
+            }).catch(error => {
+                console.log('Category update ERROR', error);
+            });
+    },
+    deleteCategory({state, commit}, id) {
+        api.delete('api/categories/' + id)
+            .then(response => {
+                commit('deleteCategory', id);
+            }).catch(error => {
+                console.log('Category delete ERROR', error.response.data);
+            });
+    },
+    createItem({state, commit}, payload) {
+        api.post('api/categories/' + state.selected_category_id + '/items', payload)
+            .then(response => {
+                commit('addItem', response.data);
+            }).catch(error => {
+                console.log('Item create ERROR', error);
+            });
+    },
+}
+
+const store = new Vuex.Store({ state, getters, mutations, actions });
+
+export default store;
